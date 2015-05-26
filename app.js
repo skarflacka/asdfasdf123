6c6690c0d391b82ce6e5012d9aa0ad0d0f85e82e
skarflacka-macbook <skarflacka232@gmail.com>
2015-05-26 17:02:43 +0200

    Handlebars.js working

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
 	var source = $("#template-test").html();
+	var source2 = $("#node1").html();
 	var template = Handlebars.compile(source);
+	var template2 = Handlebars.compile(source2);
 	var context = {title: "Lorem Ipsum"};
+
+	var data = {
+		node1: {node2: "Node2", node3: "node3", node4: {node6: "node6"}, node5: "node5"}
+	};
+
 	var output = template(context);
+	var output2 = template2(data);
 	$("#header").html(output);
+	$("#content").html(output2);
 });
